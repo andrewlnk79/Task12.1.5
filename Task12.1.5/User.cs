ebba30b46495951c4ad7df16a58530134b10c63b
Andrey Linnik <av.linnik79@ya.ru>
2023-04-15 08:09:51 +0300

    Добавьте файлы проекта.

---
+++ Task12.1.5/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12._1._5
+{
+    internal class User
+    {
+        internal string Login { get; set; }
+        internal string Name { get; set; }
+        internal bool IsPremium { get; set; } = true;
+
+    }
+
+}
